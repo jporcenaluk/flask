ffbabfb11cc20a386f2fc8ba44dd483698bdd913
Jared Porcenaluk <jporcenaluk@users.noreply.github.com>
2022-08-18 12:39:06 +0100

    Now logging actual request IP

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     
-    app.logger.info(request.remote_addr)
+    
     request_ip = request.remote_addr
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         request_ip = request.environ['REMOTE_ADDR']
     else:
         request_ip = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
+    app.logger.info("IP Address", request_ip)
     return jsonify({"IP Address": f"{request_ip}"})
 
 
