191f023bd1ec5ac2602b9945256b8a0003daa331
Jared Porcenaluk <jporcenaluk@users.noreply.github.com>
2022-08-18 12:45:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def index():
         request_ip = request.environ['REMOTE_ADDR']
     else:
         request_ip = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
-    app.logger.info("IP Address", request_ip)
+    app.logger.info(request_ip)
     return jsonify({"IP Address": f"{request_ip}"})
 
 
