59c86dd2abaf425a83a6ff0ed24b60e801f1f57a
Jared Porcenaluk <jporcenaluk@users.noreply.github.com>
2022-08-18 13:01:43 +0100

    Logging error request_ip

--- main.py
+++ main.py
@@ -9,20 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     
-    app.logger.info("Doing logs, everyone loves it")
-    logging.info("built in logs, bro")
-    print("Printing? PRintintg when it shuts down")
-    print("sriously not working")
-    logging.error("Errors are logged maybe")
-    app.logger.error("app logger errors maybe logged")
     request_ip = request.remote_addr
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         request_ip = request.environ['REMOTE_ADDR']
     else:
         request_ip = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
-    app.logger.info(request_ip)
-    return jsonify({"IP Address": f"{request_ip}"})
 
+    app.logger.error("Request IP", request_ip)
+    return jsonify({"IP Address": f"{request_ip}"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
